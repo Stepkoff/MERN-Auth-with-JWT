170e5c4f5cac0bc9bff68901d65b358e1be4c4f1
Stepan Rudiak <stepan.rudiak95@gmail.com>
2025-06-09 23:25:04 +0200

    front-start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Button, HStack } from '@chakra-ui/react';
 
+const Home = () => {
+    return (
+        <div id="home">
+            <HStack>
+                <Button>Click me 1</Button>
+                <Button>Click me 2</Button>
+            </HStack>
+        </div>
+    );
+};
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        index: true,
+        element: <Home />,
+    },
+]);
 
 export const App = () => {
-
-
-
-  return (
-    <div>
-      Init
-    </div>
-  )
-}
-
+    return (
+        <div className={'wrapper'}>
+            <RouterProvider router={router} />
+        </div>
+    );
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {App} from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App.jsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './config/queryClient.js';
+import { Provider } from '@/components/ui/provider';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Provider>
+                <App />
+            </Provider>
+            <ReactQueryDevtools
+                buttonPosition={'bottom-left'}
+                initialIsOpen={false}
+            />
+        </QueryClientProvider>
+    </StrictMode>,
+);
